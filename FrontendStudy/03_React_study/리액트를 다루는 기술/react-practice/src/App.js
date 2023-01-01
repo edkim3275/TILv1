dbfe764c80595d9e1a68ca48b54f8ad9049885ad
edkim3275 <edkim3275@gmail.com>
2023-01-01 23:53:56 +0900

    0101_리액트를 다루는 기술

--- FrontendStudy/03_React_study/리액트를 다루는 기술/react-practice/src/App.js
+++ FrontendStudy/03_React_study/리액트를 다루는 기술/react-practice/src/App.js
@@ -2,6 +2,8 @@ import { Component } from "react";
 // import MyComponent from "./MyComponent";
 import Counter from "./Counter";
 import Say from "./Say";
+import ScrollBox from "./ScrollBox";
+import ValidationSample from "./ValidationSample";
 
 class App extends Component {
   render() {
@@ -12,8 +14,14 @@ class App extends Component {
         <MyComponent name={name} favoriteNumber={3}>
           리액트 연습중
         </MyComponent> */}
-        <Counter name="edgar" />
-        <Say />
+
+        {/* <Counter name="edgar" /> */}
+        {/* <Say /> */}
+        {/* <ValidationSample /> */}
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
       </>
     );
   }
