5d4e3012f6d5f36a66c429cbdafe935c1e3f626f
edkim3275 <edkim3275@gmail.com>
2023-01-04 23:50:01 +0900

    0104_(리다기)컴포넌트 라이프사이클

--- FrontendStudy/03_React_study/리액트를 다루는 기술/react-practice/src/App.js
+++ FrontendStudy/03_React_study/리액트를 다루는 기술/react-practice/src/App.js
@@ -2,11 +2,26 @@ import { Component } from "react";
 // import MyComponent from "./MyComponent";
 import Counter from "./Counter";
 import IterationSample from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
 import Say from "./Say";
 import ScrollBox from "./ScrollBox";
 import ValidationSample from "./ValidationSample";
 
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
 class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
     // const name = "edgar";
     return (
@@ -23,7 +38,9 @@ class App extends Component {
         <button onClick={() => this.scrollBox.scrollToBottom()}>
           맨 밑으로
         </button> */}
-        <IterationSample />
+        {/* <IterationSample /> */}
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
       </>
     );
   }
